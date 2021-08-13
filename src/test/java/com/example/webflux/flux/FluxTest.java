d626342f314d9ad1cfd51c75b8f7b24b641039d9
Riyaz <riyazpasha880@gmail.com>
2021-08-13 12:46:19 +0530

    mono to flux flux to mono flux to mono -> mono to flux flatMapMany flatMapIterable

--- src/test/java/com/example/webflux/flux/FluxTest.java
+++ src/test/java/com/example/webflux/flux/FluxTest.java
@@ -6,6 +6,9 @@ import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class FluxTest {
 
@@ -78,13 +81,67 @@ public class FluxTest {
     }
 
     @Test
-    void reduceFluxToMono() {
+    void reduceFluxToMonoHasElements() {
         Mono<Boolean> fruits = Flux.just("Apples", "Bananas", "Cherries", "Dates").hasElements()
 //                .reduce(new ArrayList<String>(), (list, nextItem) -> {
 //                    list.add(nextItem);
 //                    return list;
 //                })
 //                .map(list -> !list.isEmpty())
+                .log();
+
+        fruits.subscribe(System.out::println, (err) -> System.err.println(err));
+    }
+
+    @Test
+    void reduceFluxToMono() {
+        Mono<ArrayList<String>> fruits = Flux.just("Apples", "Bananas", "Cherries", "Dates")
+                .reduce(new ArrayList<String>(), (list, nextItem) -> {
+                    list.add(nextItem);
+                    return list;
+                }).log();
+
+        fruits.subscribe(System.out::println, (err) -> System.err.println(err));
+    }
+
+    @Test
+    void reduceMonoToFlux() {
+        Flux<String> fruits = Mono.just(List.of("Apples", "Bananas", "Cherries", "Dates")).flatMapMany(Flux::fromIterable)
+//                .reduce(new ArrayList<String>(), (list, nextItem) -> {
+//                    list.add(nextItem);
+//                    return list;
+//                })
+//                .map(list -> !list.isEmpty())
+                .log();
+
+        fruits.subscribe(System.out::println, (err) -> System.err.println(err));
+    }
+
+    @Test
+    void reduceFluxToMonoThenMonoToFlux() {
+        Flux<String> fruits = Flux.just("Apples", "Bananas", "Cherries", "Dates")
+                .reduce(new ArrayList<String>(), (list, nextItem) -> {
+                    list.add(nextItem);
+                    return list;
+                })
+                .map(listOfFruits -> listOfFruits.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList()))
+                .flatMapIterable(listOfFruitsInReverseOrder -> listOfFruitsInReverseOrder)
+                .log();
+
+
+        StepVerifier.create(fruits)
+                .expectNext("Dates", "Cherries", "Bananas", "Apples")
+                .verifyComplete();
+        fruits.subscribe(System.out::println, (err) -> System.err.println(err));
+    }
+
+    @Test
+    void fluxDoNextMutation() {
+        Flux<String> fruits = Flux.just("Apples", "Bananas", "Cherries", "Dates")
+                .doOnNext(fruit -> {
+                    System.out.println("Prev value - " + fruit);
+                    fruit.toUpperCase();
+                })
                 .log();
 
         fruits.subscribe(System.out::println, (err) -> System.err.println(err));
