24aef1524bcbd0544c0f6c399a2b015a725b2efc
Riyaz <riyazpasha880@gmail.com>
2021-08-24 11:02:22 +0530

    Delay in reactive way

--- src/main/java/com/example/webflux/delay/DelayController.java
+++ src/main/java/com/example/webflux/delay/DelayController.java
@@ -1,35 +1,55 @@
 package com.example.webflux.delay;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 @RestController
+@Slf4j
 public class DelayController {
     int timeDelay = 5;
 
     @RequestMapping(value = "/polling/{count}",
             method = {RequestMethod.PUT, RequestMethod.POST, RequestMethod.GET})
-    public Mono<?> pollingEndpoint(@PathVariable int count) {
-        return waitAndGetResponse();
+    public Mono<String> pollingEndpoint(@PathVariable int count) {
+        return waitAndGetResponseMono();
+    }
+//
+//    private Mono<?> waitAndGetResponse() {
+//        Mono<?> response = null;
+//        for (int i = 0; i < timeDelay; i++) {
+//            if (response == null) sleep();
+//            else break;
+//            response = getResponse();
+//        }
+//        return response;
+//    }
+
+    private Mono<String> waitAndGetResponseMono() {
+        return getReponseWithDelay(1)
+                .expand(res -> {
+                    if (res == "dataNotPresent") return Mono.empty();
+                    return getReponseWithDelay(Integer.parseInt(String.valueOf(res.charAt(res.length() - 1))) + 1);
+                })
+                .reduce((a, b) -> a + b);
     }
 
-    private Mono<?> waitAndGetResponse() {
-        Mono<?> response = null;
-        for (int i = 0; i < timeDelay; i++) {
-            if (response == null) sleep();
-            else break;
-            response = getResponse();
-        }
-        return response;
+    private Mono<String> getReponseWithDelay(int num) {
+        return getResponse(num).delaySubscription(Duration.ofSeconds(5));
     }
 
-    private Mono<?> getResponse() {
-        return null;
+    private Mono<String> getResponse(int num) {
+        log.error("Returning response {}", num);
+        if (num > 5) return Mono.just("dataNotPresent");
+        return Mono.just("Riyaz" + num)
+                .doOnSubscribe((a) -> log.info("Subscribing {} {}", num, a))
+                .doOnNext(item -> log.info("doOnNext {}", item));
     }
 
     private void sleep() {
