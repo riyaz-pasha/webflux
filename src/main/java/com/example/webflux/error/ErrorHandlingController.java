fe4fa577a64e8acc99ebd95f65312098502a22b8
Riyaz <riyazpasha880@gmail.com>
2021-08-30 10:21:35 +0530

    Handle client exception

--- src/main/java/com/example/webflux/error/ErrorHandlingController.java
+++ src/main/java/com/example/webflux/error/ErrorHandlingController.java
@@ -1,10 +1,14 @@
 package com.example.webflux.error;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 
 @RestController
+@Slf4j
 public class ErrorHandlingController {
 
     @GetMapping("error")
@@ -15,5 +19,17 @@ public class ErrorHandlingController {
     @GetMapping("error2")
     public Mono<String> example2() {
         throw new NullPointerException("Exception thrown");
+    }
+
+    @GetMapping("error3")
+    public Mono<String> getException() {
+        Mono<String> listMono = WebClient.create("http://localhost:8080")
+                .get().uri("/error")
+                .retrieve()
+                .bodyToMono(String.class)
+                .onErrorMap(WebClientResponseException.class, throwable -> new NullPointerException("Exception thrown - " + throwable.getStatusCode()))
+                .doOnSuccess(res -> log.info("Success res " + res))
+                .doOnError(res -> log.info("Error res " + res));
+        return listMono;
     }
 }
